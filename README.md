7d55f95a6b266492350fd79f96e2e0f11de391e5
Radoslav <radmrkv@gmail.com>
2018-10-21 17:44:49 +0100

    Corrected variable names to fit the python style, and updated the readme with a to-do list.

--- README.md
+++ README.md
@@ -3,6 +3,17 @@ This is a simple utility that semi-automates combat encounters for the [Blood Sw
 
 
 ## Features
-* Attack roll simulation
-* Maintaining actor states
-* Different attacks
+* Attack roll simulation; weapon attacks and blasting spells
+* Different attacks with different attack dice and damage outputs (e.g. the Sage's Quarterstaff Technique)
+* Battlefields: sets of actors (players or enemies) that facilitate simple attacking logic
+* Maintaining actor states across rounds (HP tracking)
+* Custom statement logging (useful for leaving yourself notes)
+
+## TODO:
+* Support psychic spells
+* Support "perks" (e.g. the Trickster's Dodging Technique or status modifiers like Nighthowl)
+* Initiative tracking?
+* Time tracking (for stuff like Nighthowl)?
+* An "undo" function?
+* Pretty "help" screen that lists commands and aliases
+* Tab-completion when selecting attacks or filling in actor names.
--- simulator.py
+++ simulator.py
@@ -6,38 +6,38 @@ from enum import Enum
 
 
 class Dice:
-    def __init__(self, value=None):
+    def __init__(self, value = None):
         self.value = value or "1d6"  # it should be a string
         # Decompile value into number of dice, number of sides, and any modifiers
         regex = re.match(r"(\d*)d(\d*)([-+]?\d*)", self.value) # this is quite wonderful
-        self.numDice = int(regex.group(1) or "1")
-        self.numSides = int(regex.group(2) or "6")
+        self.num_dice = int(regex.group(1) or "1")
+        self.num_sides = int(regex.group(2) or "6")
         self.modifier = int(regex.group(3) or "0") # default value is 1d6+0
 
     # We'd also like a function that rolls the dice, right?
-    def Roll(self):
-        return self.modifier + (0 if self.numDice == 0 else
-            sum([random.randint(1, self.numSides) for _ in range(self.numDice)]))
+    def roll(self):
+        return self.modifier + (0 if self.num_dice == 0 else
+            sum([random.randint(1, self.num_sides) for _ in range(self.num_dice)]))
+
 
 #############################################
 class Attack:
     class Type(Enum):
-        Weapon = 0,
-        Blasting = 1,
-        Psychic = 2
+        Weapon = 1,
+        Blasting = 2,
+        Psychic = 3
 
-    def __init__(self, ownerActor, name=None, dice=None, damage=None, type=None, spellLevel=None):
-        self.owner = ownerActor
+    def __init__(self, owner_actor, name=None, dice=None, damage=None, attack_type=None, spell_level=None):
+        self.owner = owner_actor
         self.name = name or "Default"
         self.dice = Dice(dice) or Dice("2d6")
-        self.damage = Dice(damage) or ownerActor.damage
-        self.type = type if type is not None and isinstance(type, Attack.Type) else Attack.Type.Weapon
-        self.spellLevel = spellLevel or 0
+        self.damage = Dice(damage) or owner_actor.damage
+        self.type = attack_type if attack_type is not None and isinstance(attack_type, Attack.Type) else Attack.Type.Weapon
+        self.spellLevel = spell_level or 0
 
-    def Resolve(self):
-        roll = self.dice.Roll()
-        result = ""
-        damageRoll = self.damage.Roll()
+    def resolve(self, target_actor = None):
+        roll = self.dice.roll()
+        damage_roll = self.damage.roll()
         # Weapon attacks: roll attack dice <= FPR
         if self.type == Attack.Type.Weapon:
             result = "(ROLLED " + str(roll)
@@ -48,7 +48,7 @@ class Attack:
                 result += " = " + t + "Hit!"
             else:
                 result += " > " + t + "Miss!"
-            return (roll, damageRoll, result, roll <= self.owner.FPR)
+            return roll, damage_roll, result, roll <= self.owner.FPR
         # Blasting spells: roll attack dice + level <= PSY - numSpellsPrepared
         elif self.type == Attack.Type.Blasting:
             result = "(ROLLED " + str(roll) + " + " + str(self.spellLevel)
@@ -59,15 +59,15 @@ class Attack:
                 result += " = " + t + "Hit!"
             else:
                 result += " > " + t + "Miss!"
-            return roll, damageRoll, result, roll + self.spellLevel <= self.owner.PSY - self.owner.numSpellsPrepared
-        # Psychic spells require knowledge of the target's PSY to calculate. Todo?
+            return roll, damage_roll, result, roll + self.spellLevel <= self.owner.PSY - self.owner.numSpellsPrepared
+        # Psychic spells: roll
         else:
             # print("Invalid attack type.")
             return 0, 0, "Invalid attack type.", False
 
     def __str__(self):
-        return self.name + ", " + str(self.type) + ", dice: " + self.dice.value + ", damage: " + self.damage.value + " (level, if a spell: " + str(
-            self.spellLevel) + ")"
+        return self.name + ", " + str(self.type) + ", dice: " + self.dice.value + ", damage: " + self.damage.value + \
+               " (level, if a spell: " + str(self.spellLevel) + ")"
 
     # I know it's a hack, but I don't care
     def __repr__(self):
@@ -76,7 +76,7 @@ class Attack:
 
 #############################################
 class Actor:
-    def __init__(self, name, FPR, AWA, PSY, ARM, END, damage, attacks=None, numSpellsPrepared=None):
+    def __init__(self, name, FPR, AWA, PSY, ARM, END, damage, attacks=None, num_spells_prepared=None):
         self.name = name
         self.FPR = FPR  # Fighting Prowess
         self.AWA = AWA  # Awareness
@@ -85,7 +85,7 @@ class Actor:
         self.END = END  # (Max) Endurance
         self.currentHP = END  # Current Endurance
         self.damage = Dice(damage)  # Regular weapon damage
-        self.numSpellsPrepared = numSpellsPrepared or 0
+        self.numSpellsPrepared = num_spells_prepared or 0
         # Custom tuple parsing. Why? Because of reasons...
         if attacks is None:
             self.attacks = [Attack(self, "Default", "2d6", damage, "Weapon")]
@@ -101,33 +101,33 @@ class Actor:
 
     #
     #
-    def Attack(self, targetActor, attackName=None):
+    def attack(self, target_actor, attack_name = None):
         # Select correct attack, if available.
-        if attackName is None:
-            attackName = "Default"
-        matchingAttacks = [x for x in self.attacks if attackName.lower() in x.name.lower()]
-        # print(matchingAttacks)
-        attack = next(iter(matchingAttacks), self.attacks[0])
+        if attack_name is None:
+            attack_name = "Default"
+        matching_attacks = [x for x in self.attacks if attack_name.lower() in x.name.lower()]
+        # print(matching_attacks)
+        attack = next(iter(matching_attacks), self.attacks[0])
         if attack is None:
             print("No such attack found for actor", self.name)
             return "No such attack found for actor " + self.name
 
         # Print attack statement
-        attackString = self.name + " attacks " + targetActor.name + (
+        attack_string = self.name + " attacks " + target_actor.name + (
             ". " if attack.name == "Default" else " using " + attack.name + ". ")
-        (numRolled, damageDealt, result, attackHit) = attack.Resolve()
-        attackString += result
+        (numRolled, damageDealt, result, attackHit) = attack.resolve()
+        attack_string += result
 
         # If the attack missed, that's it.
         if not attackHit:
-            return attackString
+            return attack_string
         # But if the attack hit, subtract target armour, adjust health, and report.
-        attackDamage = damageDealt - targetActor.ARM if damageDealt - targetActor.ARM > 0 else 0
-        targetActor.currentHP -= attackDamage
-        attackString += (" Dealt " + str(damageDealt) + " - " + str(targetActor.ARM) + " = " + str(
-            attackDamage) + " damage, " +
-                         targetActor.name + " " + str(targetActor.currentHP) + "/" + str(targetActor.END) + ".")
-        return attackString
+        attack_damage = damageDealt - target_actor.ARM if damageDealt - target_actor.ARM > 0 else 0
+        target_actor.currentHP -= attack_damage
+        attack_string += (" Dealt " + str(damageDealt) + " - " + str(target_actor.ARM) + " = " + str(
+            attack_damage) + " damage, " +
+                         target_actor.name + " " + str(target_actor.currentHP) + "/" + str(target_actor.END) + ".")
+        return attack_string
 
     def __str__(self):
         return (self.name + " (" + str(self.currentHP) + "/" + str(self.END) + "): FPR " + str(
@@ -141,16 +141,16 @@ class Battlefield:
     def __init__(self, actors=None):
         self.actors = actors or []
 
-    def Add(self, actors):
+    def add(self, actors):
         self.actors.extend(actors)
 
-    def Status(self):
+    def status(self):
         print("")
         print("BATTLEFIELD STATUS:")
         for i, x in enumerate(self.actors):
             print(i, "|", x)
 
-    def GetActor(self, index):
+    def get_actor(self, index):
         #print(index)
         if index.isdigit():
             return self.actors[int(index)]
@@ -189,12 +189,13 @@ s4 = Actor("Skeleton 4", 9, 9, 9, 3, 21, "2d6")
 battlefield = Battlefield([warrior, trickster, sage, enchanter, s1, s2, s3, s4])
 
 
-def LogString(addition):
+def log_string(addition):
     with open("combatLog.txt", "w", encoding="utf-8") as file:
         file.write(addition)
         file.flush()
 
 
+# noinspection PyMethodMayBeStatic
 class Interpreter(cmd.Cmd):
     prompt = "\n>>>>> Awaiting command: "
 
@@ -207,24 +208,23 @@ class Interpreter(cmd.Cmd):
     do_EOF = do_x = do_q = do_quit = do_exit
 
     def do_status(self):
-        battlefield.Status()
+        battlefield.status()
     do_s = do_battlefield = do_status
 
     def do_log(self, arg):
-        LogString(arg + "\n")
+        log_string(arg + "\n")
 
     def do_attack(self, args):
-        #print(args)
         params = args.split(" ")
-        actor = battlefield.GetActor(params[0])
-        target = battlefield.GetActor(params[1])
+        actor = battlefield.get_actor(params[0])
+        target = battlefield.get_actor(params[1])
         attack = params[2] if len(params) >= 3 else None
         if actor is None or target is None:
             print("Invalid combatants specified. Try again.")
             return False
         # If all's well, do the attack
         result = actor.Attack(target, attack)
-        LogString(result + "\n")
+        log_string(result + "\n")
         print(result)
     do_a = do_attack
 
@@ -244,15 +244,15 @@ class Interpreter(cmd.Cmd):
         pass
 
     def default(self, line):
-        cmd, arg, line = self.parseline(line)
-        print(f'Invalid command: "{cmd}"')
+        command, arg, line = self.parseline(line)
+        print('Invalid command: "' + command + '"')
 
 
 print("")
 print("BLOOD SWORD COMBAT SIMULATOR")
 print("(created by Lumos, 2018)")
 print("Edit the script file to set up your battlefield with greater ease.")
-battlefield.Status()
+battlefield.status()
 interpreter = Interpreter()
 interpreter.cmdloop()
 
