aada9afef2c2d75135e4ce92cc6be5cc050129a2
Radoslav <radmrkv@gmail.com>
2018-11-21 01:24:19 +0000

    Basic perk support added, but they're not taken into account at all proper places yet. Todo perk "str" method as well.

--- sim_util.py
+++ sim_util.py
@@ -2,15 +2,27 @@ import re
 import random
 
 
+#############################################
 class Dice:
-    def __init__(self, value = None):
+    def __init__(self, value=None):
         self.value = value or "1d6"  # it should be a string
         # Decompile value into number of dice, number of sides, and any modifiers
         regex = re.match(r"(-?)(\d*)d([-+]?\d*)([-+]?\d*)", self.value)  # this is quite wonderful
-        self.minus = -1 if regex.group(1) else 1
-        self.num_dice = int(regex.group(2) or "1")
-        self.num_sides = int(regex.group(3) or "0")
-        self.modifier = int(regex.group(4) or "0")  # default value is 1d0+0
+        if regex is not None:
+            self.minus = -1 if regex.group(1) else 1
+            self.num_dice = int(regex.group(2) or "1")
+            self.num_sides = int(regex.group(3) or "0")
+            self.modifier = int(regex.group(4) or "0")  # default value is 1d0+0
+        # However, if it's a single digit instead, just set the modifier accordingly
+        else:
+            regex = re.match("(-?\d+)", self.value)
+            if regex is None:
+                raise AttributeError("A dice object cannot be initialised with a value of '" + str(value) + "'")
+            else:
+                self.modifier = regex.group(1)
+                self.minus = 1
+                self.num_dice = 0
+                self.num_sides = 0
 
     # We'd also like a function that rolls the dice, right?
     def roll(self):
@@ -21,19 +33,28 @@ class Dice:
 
 
 #############################################
-# A perk is applied to an actor and modifies THIS actor's pytho
+# A perk is applied to an actor and modifies THIS actor's properties.
 class Perk:
-    def __init__(self, FPR=None, AWA=None, PSY=None, ARM=None, END=None, damage=None, turns=None):
-        self.turns_left = turns or 1500000 # If no turns have been specified, make it "eternal"
-        self.modifiers = {
-            "fpr": FPR or 0, # Fighting Prowess
-            "awa": AWA or 0, # Awareness
-            "psy": PSY or 0, # Psychic Ability
-            "end": END or 0, # Endurance
-            "armour": ARM or 0, # Armour
-            "damage": damage or Dice("0d0") # Extra damage?
+    def __init__(self, name, fpr=None, awa=None, psy=None, armour=None, end=None,
+                 to_hit=None, damage=None, turns=None, extra_dodge=None):
+        self.name = name
+        self.turns_remaining = turns or 1500000  # If no turns have been specified, make it "eternal"
+        self.values = {
+            "fpr": fpr or 0,  # Fighting Prowess
+            "awa": awa or 0,  # Awareness
+            "psy": psy or 0,  # Psychic Ability
+            "end": end or 0,  # Endurance
+            "armour": armour or 0,  # Armour
+            "to_hit": to_hit or Dice("0"),  # To hit penalties for Nighthowl
+            "damage": damage or Dice("0"),  # Extra damage or damage penalties
+            "extra_dodge": extra_dodge or Dice("0"),  # For stuff like Dodging Technique
         }
 
+    # TODO: define the ToString method.
+    # e.g. "Dodging Technique (Dodge +1)" or "Prepared Spells (PSY -1)"
+
+
+#############################################
 # This converts lists of positive and negative ints to a pretty string.
 def prettify_ints(int_list):
     int_list = int_list if isinstance(int_list, list) else [int_list]
@@ -46,17 +67,39 @@ def prettify_ints(int_list):
     return result
 
 
+# Use this to make defining "attack tuple notation" easier to read.
+# Tried named tuples, but their optional parameters are version dependent or some other bullshit, not worth it.
+# noinspection PyPep8Naming
+def Attack(name, to_hit=None, damage=None, attack_type=None, spell_level=None):
+    return name, to_hit, damage, attack_type, spell_level
 
 
-class Attack:
-    def __init__(self, owner_actor, name=None, dice=None, damage=None, attack_type=None, spell_level=None):
+class AttackClass:
+    def __init__(self, owner_actor, name=None, to_hit=None, damage=None, attack_type=None, spell_level=None):
         self.owner = owner_actor
         self.name = name or "Default"
-        self.dice = Dice(dice) or Dice("2d6")
+        self.to_hit = Dice(to_hit) or Dice("2d6")
         self.damage = Dice(damage) or owner_actor.damage
         self.type = attack_type or "Weapon"
         self.spellLevel = spell_level or 0
 
+    # Easier generator for actor constructors.
+    @classmethod
+    def default_attack(cls, owner_actor, default_damage):
+        return AttackClass(owner_actor, "Default", "2d6", default_damage, "Weapon")
+
+    # This one parses "attack tuple notation", again to make actor constructors cleaner.
+    @classmethod
+    def parse_tuple(cls, owner_actor, default_damage, x):
+        return AttackClass(owner_actor,
+                           ("Default"      if x[0] is None else x[0]),  # Name
+                           ("2d6"          if x[1] is None else x[1]),  # Dice roll
+                           (default_damage if x[2] is None else x[2]),  # Damage
+                           ("Weapon"       if x[3] is None else x[3]),  # Type
+                           (0              if x[4] is None else x[4]))  # Type
+
+    #
+    #
     def resolve(self, target_actor=None):
         # Now break this up into functions to make it more palatable,
         if self.type == "Weapon":
@@ -69,6 +112,7 @@ class Attack:
             print("Invalid attack type specified:", self.type)
             return
 
+    #
     # Generic function for the "(ROLLED X <=> Y)" output we need for everything.
     # Returns true if the roll is equal to or under the target
     def __compute_roll(self, rolled_vals, target_vals):
@@ -80,21 +124,24 @@ class Attack:
             sign = "<"
         elif roll_total > roll_target:
             sign = ">"
-        roll_string = "(ROLLED " + prettify_ints(rolled) + " " + sign + " " + prettify_ints(target) + ")"
+        roll_string = "(ROLLED " + prettify_ints(rolled_vals) + " " + sign + " " + prettify_ints(target_vals) + ")"
         return True if sign == "=" or sign == "<" else False, roll_string
 
-    # Weapon attacks: roll attack dice + target perks <= own FPR + own perks; affected by armour
+    #
+    # Weapon attacks: roll attack dice + target perks dodge <= own FPR + own perks FPR; affected by armour
     def __weapon_attack(self, target_actor):
         # Roll to hit and damage.
-        to_hit = self.dice.roll()
-
+        to_hit = self.to_hit.roll()
+        damage = self.damage.roll()
         # Add all of our modifiers to this
+
 
         hit, string = __compute_roll()
 
 
         return ""
 
+    #
     # Blasting spells: roll attack dice + spell level <= own PSY - spells prepped; affected by armour
     def __blasting_attack(self, target_actor, to_hit, damage):
         return ""
@@ -103,47 +150,8 @@ class Attack:
     def __psychic_attack(self, target_actor, damage):
         return "Not implemented."
 
-
-
-
-
-
-
-
-
-
-
-
-
-        # Weapon attacks: roll attack dice <= FPR
-        if self.type == Attack.Type.Weapon:
-            result = "(ROLLED " + str(roll)
-            t = str(self.owner.FPR) + ") "
-            if roll < self.owner.FPR:
-                result += " < " + t + "Hit!"
-            elif roll == self.owner.FPR:
-                result += " = " + t + "Hit!"
-            else:
-                result += " > " + t + "Miss!"
-            return roll, damage_roll, result, roll <= self.owner.FPR
-        # Blasting spells: roll attack dice + level <= PSY - numSpellsPrepared
-        elif self.type == Attack.Type.Blasting:
-            result = "(ROLLED " + str(roll) + " + " + str(self.spellLevel)
-            t = str(self.owner.PSY) + " - " + str(self.owner.numSpellsPrepared) + ") "
-            if roll + self.spellLevel < self.owner.PSY - self.owner.numSpellsPrepared:
-                result += " < " + t + "Hit!"
-            elif roll + self.spellLevel == self.owner.PSY - self.owner.numSpellsPrepared:
-                result += " = " + t + "Hit!"
-            else:
-                result += " > " + t + "Miss!"
-            return roll, damage_roll, result, roll + self.spellLevel <= self.owner.PSY - self.owner.numSpellsPrepared
-        # Psychic spells: roll
-        else:
-            # print("Invalid attack type.")
-            return 0, 0, "Invalid attack type.", False
-
     def __str__(self):
-        return self.name + ", " + str(self.type) + ", dice: " + self.dice.value + ", damage: " + self.damage.value + \
+        return self.name + ", " + str(self.type) + ", dice: " + self.to_hit.value + ", damage: " + self.damage.value + \
                " (level, if a spell: " + str(self.spellLevel) + ")"
 
     # I know it's a hack, but I don't care
@@ -153,39 +161,45 @@ class Attack:
 
 #############################################
 class Actor:
-    def __init__(self, name, FPR, AWA, PSY, ARM, END, damage, attacks=None, perks=None, num_spells_prepared=None):
+    def __init__(self, name, fpr, awa, psy, armour, end, damage, attacks=None, perks=None, spells_prepared=None):
         self.name = name
+        # Parse basic attributes
         self.attributes = {
-            "fpr": FPR, # Fighting Prowess
-            "awa": AWA, # Awareness
-            "psy": PSY, # Psychic Ability
-            "end": END, # Endurance (HP)
-            "armour": ARM, # Armour
-            "damage": damage # Basic "damage per blow"
+            'fpr': fpr,  # Fighting Prowess
+            'awa': awa,  # Awareness
+            'psy': psy,  # Psychic Ability
+            'end': end,  # Base Endurance (max HP before any modifiers)
+            'armour': armour,  # Armour
+            'damage': Dice(damage)  # Default weapon damage
         }
-
-
-        self.FPR = FPR  # Fighting Prowess
-        self.AWA = AWA  # Awareness
-        self.PSY = PSY  # Psychic Ability
-        self.ARM = ARM  # Armour rating
-        self.END = END  # (Max) Endurance
-        self.currentHP = END  # Current Endurance
-        self.damage = Dice(damage)  # Regular weapon damage
-        self.numSpellsPrepared = num_spells_prepared or 0
-        self.perks = perks  # "Perks" currently active
-        # Custom tuple parsing. Why? Because of reasons...
+        # Now parse attacks.
         if attacks is None:
-            self.attacks = [Attack(self, "Default", "2d6", damage, "Weapon")]
+            self.attacks = [AttackClass.default_attack(self, damage)]
         else:
-            self.attacks = [Attack(self,
-                                   ("Default"           if len(x) < 1 or x[0] is None else x[0]),  # Name
-                                   ("2d6"               if len(x) < 2 or x[1] is None else x[1]),  # Dice roll
-                                   (damage              if len(x) < 3 or x[2] is None else x[2]),  # Damage
-                                   (Attack.Type.Weapon  if len(x) < 4 or x[3] is None else Attack.Type[x[3]]),
-                                   (0                   if len(x) < 5 or x[4] is None else x[4]))  # Type
-                            for x in attacks]
-            # print(self.attacks)
+            self.attacks = [AttackClass.parse_tuple(self, damage, x) for x in attacks]
+        # And perks. If it's a list of perks, use it; if it's a single perk, make it a list; else ignore
+        self.perks = []
+        if type(perks) is list:
+            self.perks = perks
+        elif isinstance(perks, Perk):
+
+            self.perks = [perks]
+        # Spells prepared: Rig this as a perk, because why not?
+        if spells_prepared is not None and spells_prepared > 0:
+            self.perks.append(Perk("Prepared Spells", psy=-1 * spells_prepared))
+
+        # Current endurance is last, taking all perks into account.
+        total_endurance = sum(self.list_attribute('end'))
+        self.end = total_endurance
+        self.currentHP = total_endurance
+
+    #
+    #
+    # Produces a list of all modifiers of a particular attribute.
+    def list_attribute(self, attribute):
+        result = [self.attributes.get(attribute)] + [x.values.get(attribute) for x in self.perks]
+        # Filter out all zeroes, but return a single zero if the list is empty (all were zeroes)
+        return list(filter(None, result)) or [0]
 
     #
     #
@@ -218,10 +232,20 @@ class Actor:
         return attack_string
 
     def __str__(self):
-        return (self.name + " (" + str(self.currentHP) + "/" + str(self.END) + "): FPR " + str(
-            self.FPR) + ", AWA " + str(self.AWA) +
-                ", PSY " + str(self.PSY) + "; Damage " + str(self.damage.value) + ", Armour rating " + str(self.ARM) +
-                ("" if self.numSpellsPrepared == 0 else ", spells prepared: " + str(self.numSpellsPrepared)))
+        # Prepared spells are special
+        spells_prepared = next((x.values.get('psy', 0) * -1 for x in self.perks if x.name == "Prepared Spells"), 0)
+        # And so is damage, because it consists of "Dice" objects.
+        total_damage = " + ".join([x.value for x in self.list_attribute('damage') if x != 0 and x.value != "0"])
+        # And also perks: if we're affected by any perks, list them.
+        perks = "" if len(self.perks) == 0 else "\n    Affected by: " + ", ".join(map(lambda x: x.name, self.perks))
+
+        return (self.name + " (" + str(self.currentHP) + "/" + str(self.end) + ")" +
+                ", FPR " + prettify_ints(self.list_attribute('fpr')) +
+                ", AWA " + prettify_ints(self.list_attribute('awa')) +
+                ", PSY " + prettify_ints(self.list_attribute('psy')) +
+                "; Damage " + total_damage +
+                ", Armour rating " + prettify_ints(self.list_attribute('armour')) +
+                ("" if spells_prepared == 0 else ", spells prepared: " + str(spells_prepared))) + perks
 
 
 #############################################
--- simulator.py
+++ simulator.py
@@ -2,41 +2,45 @@ import cmd
 import os
 from sim_util import *
 
-##############################################################################################################
-
-warrior = Actor("Sir Richard", 10, 7, 6, 3, 25, "2d6", [
-    ("the Enchanted Sword", None, None, None),
-])
-trickster = Actor("Trickster", 7, 8, 7, 2, 24, "1d6+2", [
-    ("his sword", None, None, None),
-    ("the Dagger of Vislet", "2d6", "1d6", "Weapon"),
-    ("his bow", "2d6", "1d6", "Weapon"),
-])
-sage = Actor("Mentok", 7, 7, 8, 3, 20, "1d6+2", [
-    ("his quarterstaff", None, None, None),
-    ("the Magic Bow", "2d6", "1d6+1", "Weapon"),
-    ("Quarterstaff Technique", "2d8", "2d6+2", "Weapon"),
-])
-enchanter = Actor("Enchanter", 4, 7, 12, 2, 20, "1d6+1", [
-    ("his sword", None, None, None),
-    ("Swordthrust", "2d6", "3d6+3", "Blasting", 2),
-    ("Nemesis Bolt", "2d6", "7d6+7", "Blasting", 5),
-], 1)
-
-battlefield = Battlefield([warrior, trickster, sage, enchanter,
-    Actor("Thulander 1", 8, 7, 6, 0, 30, "2d6+1"),
-    Actor("Thulander 2", 8, 7, 6, 0, 30, "2d6+1"),
-])
 
 ##############################################################################################################
+# Arguments have names listed to make it easier to remember. It's also more cluttered, but what can one do...
+warrior = Actor("Sir Richard", fpr=10, awa=7, psy=6, armour=3, end=25, damage="2d6", attacks=[
+    Attack("the Enchanted Sword"),
+])
+trickster = Actor("Trickster", fpr=7, awa=8, psy=7, armour=2, end=24, damage="1d6+2", attacks=[
+    Attack("his sword"),
+    Attack("the Dagger of Vislet", to_hit="2d6", damage="1d6", attack_type="Weapon"),
+    Attack("his bow", "2d6", "1d6", "Weapon"),
+], perks=[
+    Perk("Dodging Technique", extra_dodge=1)
+])
+sage = Actor("Mentok", fpr=7, awa=7, psy=8, armour=3, end=20, damage="1d6+2", attacks=[
+    Attack("his quarterstaff"),
+    Attack("the Magic Bow", to_hit="2d6", damage="1d6+1", attack_type="Weapon"),
+    Attack("Quarterstaff Technique", "2d8", "2d6+2", "Weapon"),
+])
+enchanter = Actor("Enchanter", fpr=4, awa=7, psy=12, armour=2, end=20, damage="1d6+1", attacks=[
+    Attack("his sword"),
+    Attack("Swordthrust", to_hit="2d6", damage="3d6+3", attack_type="Blasting", spell_level=2),
+    Attack("Nemesis Bolt", "2d6", "7d6+7", "Blasting", 5),
+], spells_prepared=1)
 
+battlefield = Battlefield([
+    warrior, trickster, sage, enchanter,
+    Actor("Thulander 1", fpr=8, awa=7, psy=6, armour=0, end=30, damage="2d6+1"),
+    Actor("Thulander 2", fpr=8, awa=7, psy=6, armour=0, end=30, damage="2d6+1"),
+])
+
+
+##############################################################################################################
 def log_string(addition):
     with open("combatLog.txt", "a+", encoding="utf-8") as file:
         file.write(addition)
         file.flush()
 
 
-# noinspection PyMethodMayBeStatic
+# noinspection PyMethodMayBeStatic,PyUnusedLocal,SpellCheckingInspection
 class Interpreter(cmd.Cmd):
     prompt = "\n>>>>> Awaiting command: "
 
@@ -49,6 +53,7 @@ class Interpreter(cmd.Cmd):
     do_EOF = do_x = do_q = do_quit = do_exit
 
     def do_status(self):
+        """Displays the current status of the battlefield."""
         battlefield.status()
     do_s = do_battlefield = do_status
 
@@ -75,12 +80,13 @@ class Interpreter(cmd.Cmd):
     do_a = do_attack
 
     def do_clear(self, _):
+        """Clears the screen from previous application output."""
         os.system('cls' if os.name == 'nt' else 'clear')
     do_cls = do_clear
 
     def do_help(self, arg):
         """List available commands."""
-        # TODO fix aliases up somehow
+        # TODO This needs to be replaced completely with something that looks better.
         cmd.Cmd.do_help(self, arg)
     do_h = do_help
 
